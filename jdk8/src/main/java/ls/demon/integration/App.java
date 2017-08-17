ee7f08ef751aea4d27e61de695363c4979218625
lisong <song.li@witontek.com>
2017-08-17 17:00:57 +0800

    add

---
+++ jdk8/src/main/java/ls/demon/integration/App.java
@@ -0,0 +1,11 @@
+package ls.demon.integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
