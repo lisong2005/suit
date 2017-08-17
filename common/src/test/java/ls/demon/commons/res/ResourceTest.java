500c0e22d900c7a5c9f8d70a2732434ee2123db8
lisong <song.li@witontek.com>
2017-08-17 23:07:29 +0800

    add

--- common/src/test/java/ls/demon/commons/res/ResourceTest.java
+++ common/src/test/java/ls/demon/commons/res/ResourceTest.java
@@ -25,11 +25,13 @@ public class ResourceTest {
     @Test
     public void test_mbg_create() {
         try {
+            ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+
             logger.info("{}", BaseModel.class.getClassLoader());
             logger.info("{}", BaseModel.class.getClassLoader().getParent());
             logger.info("{}", BaseModel.class.getClassLoader().getParent().getParent());
             logger.info("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-            logger.info("{}", Thread.currentThread().getContextClassLoader());
+            logger.info("{}", contextClassLoader);
             logger.info("{}", Thread.currentThread().getClass().getClassLoader());
             logger.info("{}", Thread.class.getClassLoader());
             logger.info("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
@@ -48,13 +50,14 @@ public class ResourceTest {
 
             logger.info("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 
-            logger.info("{}",
-                Thread.currentThread().getContextClassLoader().getResource("logback.xml"));
-            logger.info("{}",
-                Thread.currentThread().getContextClassLoader().getResource("app.properties"));
+            logger.info("{}", contextClassLoader.getResource("logback.xml"));
+            logger.info("{}", contextClassLoader.getResource("app.properties"));
+            logger.info("{}", contextClassLoader.getResource("logback-test.xml"));
+            logger.info("{}", contextClassLoader.getResource("app-test.properties"));
 
             logger.info("{}", BaseModel.class.getResource("app.properties"));
             logger.info("{}", BaseModel.class.getResource("/app.properties"));
+            logger.info("{}", BaseModel.class.getResource("/app-test.properties"));
 
             logger.info("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
             ClassPathResource r = new ClassPathResource("/app.properties");
@@ -69,12 +72,14 @@ public class ResourceTest {
     @Test
     public void test2() {
         try {
-            logger.info("{}", BaseModel.class.getClassLoader().getResource("app.properties"));
+            ClassLoader classLoader = BaseModel.class.getClassLoader();
+            ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+            logger.info("{}", contextClassLoader.getResource("app.properties"));
+            logger.info("{}", classLoader.getResource("app.properties"));
 
-            logger.info("{}", BaseModel.class.getClassLoader()
+            logger.info("{}", classLoader
                 .getResource("org/springframework/web/servlet/config/spring-mvc-3.0.xsd"));
-            logger.info("{}",
-                Thread.currentThread().getContextClassLoader().getResource("app.properties"));
+            logger.info("{}", classLoader.getResource("org/springframework/util/Assert.class"));
 
             ClassPathResource r = new ClassPathResource("/app.properties");
             logger.info("{}", r);
