7d6f2687cf640c6ee7d19e3ba2657927a1d1a79a
lisong <song.li@witontek.com>
2017-09-01 12:06:53 +0800

    add

---
+++ springboot/src/test/java/ls/demon/integration/AppTest.java
@@ -0,0 +1,42 @@
+package ls.demon.integration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+    /**
+    * Logger for this class
+    */
+    private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+        logger.info("xxx");
+    }
+}
